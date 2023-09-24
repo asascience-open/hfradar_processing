979ac92726a66f9aa057e13c5ff81bdc7ab05876
Benjamin Adams <ben.adams@rpsgroup.com>
2023-09-24 13:57:12 -0400

    Add datetime column to HFRadar CSV files

--- lambda/fetch_and_split_hfradar.py
+++ lambda/fetch_and_split_hfradar.py
@@ -2,6 +2,7 @@ import re
 import sys
 import logging
 import io
+import datetime
 import urllib.parse
 import boto3
 
@@ -14,6 +15,15 @@ def output_files(bucket: str, site: str, measurement_type: str, filename: str,
     is_footer = False
     for line in file_handle:
         # match headers
+        if maybe_match_ts := re.match(r"(?<=%TimeStamp: )\d{4}, \d{2} \d{2}"
+                                    r"  \d{2} \d{2} \d{2}",
+                                    line):
+            timestamp_str = maybe_match_ts.group(0)
+        if maybe_match_tz := re.match(r'(?<TimeZone: [^"]+]', line):
+            timezone_str = maybe_match_tz.group(0)
+            timestamp_with_tz_string = str(datetime.strptime("%Y %m %d"
+                                                          " %H %M %S %Z"))
+
         if not re.match(r"( |%%\s+(Longitude|\(deg\)))", line):
             header_footer_lines[is_footer].append(line)
         else:
@@ -22,16 +32,19 @@ def output_files(bucket: str, site: str, measurement_type: str, filename: str,
             if not line.startswith("%%"):
                 processed_line = re.sub("(?<![A-Z])\s+(?!$)", ",", line.strip()
                                         + "\n")
+                data_lines.append(processed_line)
             else:
                 processed_line = line
-            data_lines.append(processed_line)
+                data_lines.append(
+                    f"{timestamp_with_tz_string},{processed_line}")
     file_name_base = filename
     # header lines can't reliably be parsed, use short names
     data_lines.insert(0,
+                      "timestamp," +
                       header_footer_lines[0][-3][19:].strip().replace(" ", ",")
                       + "\n")
     base_path = f"{site}/{measurement_type}_processed/{filename}"
-    
+
     # TODO: DRY up/ partial function application
     s3.put_object(Bucket=bucket, Key=f"{base_path}.header.txt",
               Body="".join(header_footer_lines[0]))
@@ -51,7 +64,7 @@ def lambda_handler(event, context):
         print(f"Unable to split into site and type, key was f{key}.")
         logging.exception(f"Unable to split into site and type, key was f{key}.")
         raise
-    # avoid looping on S3 file 
+    # avoid looping on S3 file
     if measurement_type.endswith("processed"):
         return
     try:
